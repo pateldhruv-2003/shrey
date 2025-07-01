d2a68f8b042273c92e7255f4edfac0cc651f59c0
pateldhruv-2003 <pateldhruv012003@gmail.com>
2025-07-01 11:58:46 +0530

    first commit

--- shrey/urls.py
+++ shrey/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from.import views
 urlpatterns = [
-    path('index', views.index),
+    path('', views.index),
     path('showstudentlist',views.showstudentlist,name="showstudentlist"),
     path('showproductlist',views.showproductlist,name="showproductlist"),
 ]
